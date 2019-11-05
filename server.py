9e1c2f70e69977f51757b31f2d42299fbcf13869
tanacchi <q111026d@mail.kyutech.jp>
2019-11-05 15:53:17 +0900

    Add function to write results of analysis

--- server.py
+++ server.py
@@ -160,6 +160,14 @@ def uploads_file():
         print("get request")
         return render_template('index.html', now_loading=now_loading)
 
+def write_analysis_result(filepath, results):
+    filename = os.path.basename(filepath)
+    with open(os.path.join("./results", filename+".txt"), mode='w') as f:
+        for key, value in results.items():
+            result_str = "{}:{}".format(key, value)
+            f.write(result_str)
+
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/uploads/<filename>')
